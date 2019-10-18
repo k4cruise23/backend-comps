6795d93b910c5bdfb3106baa5d458cc21dc1f8bd
Katie Cruise <katiecruise23@gmail.com>
2019-10-18 16:45:23 -0600

    completed competencies

--- db/many_many.sql
+++ db/many_many.sql
@@ -1,14 +1,42 @@
-CREATE TABLE favorite_foods (
-food_id SERIAL PRIMARY KEY,
-food_name TEXT,
-user_id INT
-);
-create Table people (
-people_id SERIAL PRIMARY KEY,
-person_name TEXT,
-person_id INT
+-- CREATE TABLE favorite_foods (
+-- food_id SERIAL PRIMARY KEY,
+-- food_name TEXT,
+-- user_id INT
+-- );
+-- create Table people (
+-- people_id SERIAL PRIMARY KEY,
+-- person_name TEXT,
+-- person_id INT
+-- );
+
+-- SELECT * FROM favorite_foods f
+-- JOIN people p ON P.person_id = f.user_id;
+
+CREATE TABLE student (
+    student_id SERIAL PRIMARY KEY,
+    first_name VARCHAR(50),
+    last_name VARCHAR(50)
 );
 
+CREATE TABLE classes (
+    class_id SERIAL PRIMARY KEY,
+    name VARCHAR(50),
+    teacher VARCHAR(50)
+);
 
-SELECT * FROM favorite_foods f
-JOIN people p ON P.person_id = f.user_id;
+CREATE TABLE student_classes (
+    class_id FOREIGN KEY REFERENCES classes,
+    student_id FOREIGN KEY REFERENCES student
+);
+
+--Getting all students for a class?
+SELECT s.student_id, last_name
+FROM student_classes sc 
+INNER JOIN students s ON s.student_id = sc.student_id
+WHERE sc.class_id = ${1}
+
+--Getting all classes for a student?
+SELECT c.class_id, name
+FROM student_classes sc 
+INNER JOIN classes c ON c.class_id = sc.class_id
+WHERE sc.student_id = ${1}
--- server/server.js
+++ server/server.js
@@ -10,6 +10,25 @@ app.use(express)
 app.use( express.static( `${__dirname}/../build` ) );
 app.use(express.json())
 
+// top level middleware - express method that runs the passed in function on every request that comes to your server
+app.use('/api/users', function(req, res, next) {
+    res.send()
+})
+
+
+//request-level middleware - will be run on every request made to this endpoint. Should have a next() to allow next function to run
+function isAdmin(req,res,next) {
+    if(!req.usAuthenticated()){
+        return res.redirect('/login')
+    } else if (!req.user.admin) {
+        return res.status(403).send({error: 'not an admin'})
+    }
+    next()
+}
+
+app.get('/api/state_secrets', isAdmin, function(req, res, next){
+    res.status(200).send(stateSecrets)
+})
 
 massive(CONNECTION_STRING).then(db=> {
     app.set('db',db)
